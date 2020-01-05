d372a1d884b68982ed60f500ed0dbb2fd49bd597
dengbo01 <dengbo01@baidu.com>
2020-01-05 18:20:37 +0800

    update

--- public/scripts/register.js
+++ public/scripts/register.js
@@ -7,7 +7,7 @@ const subscribeOptions = {
     )
 };
 
-function urlB64ToUint8Array(base64String) {
+function urlBase64ToUint8Array(base64String) {
     const padding = '='.repeat((4 - base64String.length % 4) % 4);
     const base64 = (base64String + padding)
         .replace(/\-/g, '+')
